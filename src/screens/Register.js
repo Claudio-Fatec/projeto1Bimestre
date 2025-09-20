be939f3127b6ce0e5470ca89a9cc3f1b1b94c920
Claudio-Fatec <162808729+Claudio-Fatec@users.noreply.github.com>
2025-09-20 19:18:47 -0300

    corrigido pagina register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,47 +1,46 @@
-import react, { use } from "react";
-import React, {useState} from "react";
-import {View, Text, TextInput,Button,StyleSheet,Alert} from 'react-native';
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Register({navigation}) {
-    const [nome, setNome] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [cpf, setCpf] = useState('');
-    const [email, setEmail] = useState('');
-    const [curso, setCurso] = useState('');
-    const [senha,setSenha] = useState('');
+export default function Register({ navigation }) {
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [curso, setCurso] = useState('');
+  const [senha, setSenha] = useState('');
 
-    async function handleSalvar() {
-        if (!nome || !email) {
-            Alert,alert('Erro','Preencha pelo menos o E-mail.');
-            return;
-        }
-        const user = {nome,telefone,cpf,email,curso,senha};
-        try {
-            await AsyncStorage.setItem('user', JSON,stringify(user));
-            Alert,alert('Sucesso', 'Usuário salvo localmente.');
-            navigation.goBack();            
-        }catch (e){
-            Alert,alert('Erro','Falha ao salvar usuário.');
-        }
+  async function handleSalvar() {
+    if (!nome || !email) {
+      Alert.alert('Erro', 'Preencha pelo menos Nome e E-mail.');
+      return;
     }
+    const user = { nome, telefone, cpf, email, curso, senha };
+    try {
+      await AsyncStorage.setItem('user', JSON.stringify(user));
+      Alert.alert('Sucesso', 'Usuário salvo localmente.');
+      navigation.goBack(); // volta ao LOGIN
+    } catch (e) {
+      Alert.alert('Erro', 'Falha ao salvar usuário.');
+    }
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Cadastrar Usuário</Text>
-            <TextInput placeholder="Nome" style={styles.input} value={nome} onChangeText={setNome}/>
-            <TextInput placeholder="Telefone" style={styles.input} value={telefone} onChangeText={setTelefone}/>
-            <TextInput placeholder="CPF" style={styles.input} value={cpf} onChangeText={setCpf}/>
-            <TextInput placeholder="E-mail" style={styles.input} keyboardType="email-address" value={email} onChangeText={setEmail}/>
-            <TextInput placeholder="Curso" style={styles.input} value={curso} onChangeText={setCurso}/>
-            <TextInput placeholder="Senha" style={styles.input} secureTextEntry value={senha} onChangeText={setSenha}/>
-            <Button title="SALVAR" onPress={handleSalvar}/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Cadastrar Usuário</Text>
+      <TextInput placeholder="Nome" style={styles.input} value={nome} onChangeText={setNome}/>
+      <TextInput placeholder="Telefone" style={styles.input} value={telefone} onChangeText={setTelefone}/>
+      <TextInput placeholder="CPF" style={styles.input} value={cpf} onChangeText={setCpf}/>
+      <TextInput placeholder="E-mail" style={styles.input} keyboardType="email-address" value={email} onChangeText={setEmail}/>
+      <TextInput placeholder="Curso" style={styles.input} value={curso} onChangeText={setCurso}/>
+      <TextInput placeholder="Senha" style={styles.input} secureTextEntry value={senha} onChangeText={setSenha}/>
+      <Button title="SALVAR" onPress={handleSalvar}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{flex:1, padding:20},
-    tittle:{fontSize:20, marginBottom:12, textAlign:'center'},
-    input:{borderWidth:1, borderColor:'#ccc',padding:10, marginBottom:10, borderRadius:6}
+  container:{ flex:1, padding:20 },
+  title:{ fontSize:20, marginBottom:12, textAlign:'center' },
+  input:{ borderWidth:1, borderColor:'#ccc', padding:10, marginBottom:10, borderRadius:6 }
 });
