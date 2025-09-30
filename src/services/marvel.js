1db5498cddce8adaeadd1538ed766f63db468af1
Claudio-Fatec <162808729+Claudio-Fatec@users.noreply.github.com>
2025-09-30 09:31:15 -0300

    Update marvel.js
    Alterar linha de chaves privadas e publicas

--- src/services/marvel.js
+++ src/services/marvel.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import * as CryptoJS from "crypto-js";
 import Constants from "expo-constants";
 
-const PUBLIC_KEY = Constants.expoConfig.extra.MARVEL_PUBLIC_KEY;'2f65f392a66b583f3f750e4bcdb562c3'
-const PRIVATE_KEY = Constants.expoConfig.extra.MARVEL_PRIVATE_KEY;'f4c6d77d8e4c94d1c14c02a15269d9f1fcfe86a4'
+const PUBLIC_KEY = '2f65f392a66b583f3f750e4bcdb562c3'
+const PRIVATE_KEY ='f4c6d77d8e4c94d1c14c02a15269d9f1fcfe86a4'
 
 export async function fetchCharacters({ limit = 10, offset = 0, name }) {
   const ts = Date.now().toString();
